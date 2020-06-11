f973e18e9ab09b071d7254c4fbe0e87f5d33cff9
rwe0214 <chiuwiy@gmail.com>
2020-06-11 15:44:32 +0800

    Change the disasm from binasii to capstone lib
    The binasii's x86_64 achetecture has a little different from the capstone's,
    and the latter is actually what I need.

--- ptrace/capstone/solver.py
+++ ptrace/capstone/solver.py
@@ -1,41 +1,32 @@
 from pwn import *
 import binascii as b
+from capstone import *
 
 def xxx(s):
-	tmp = ''
-	for i in range(len(s)-1):
-		if s[i] == ' ' and s[i+1] == ' ':
-			continue
-		else:
-			tmp += s[i]
-	tmp += s[-1]
-	tmp += '\n'
 	ans = ''
-	for c in tmp:
+	for c in s:
 		ans += '{:02x}'.format(int(ord(c)))
 	return ans
 
 conn = remote('aup.zoolab.org', 2530)
-context.clear()
-context.arch = 'x86_64'
+md = Cs(CS_ARCH_X86, CS_MODE_64)
 
 for i in range(10):
-	print(conn.recvuntil(b'> ').decode('utf-8'), end='')
+	conn.recvuntil(b'> ')
 	quiz = conn.recvline(keepends=False)
-	print(quiz.decode('utf-8'))
+	
 	#convert into binary format
 	b_quiz = b.a2b_hex(quiz)
 
 	#disasm
-	asm = disasm(b_quiz, byte=0, offset=0)
+	asm = ''
+	for inst in md.disasm(b_quiz, 0):
+		asm += (inst.mnemonic + ' ' + inst.op_str + '\n')
 
 	#revise output form
 	ans = xxx(asm)
 	conn.sendline(ans)
 	print(conn.recvline(keepends=False).decode('utf-8'))
-	if conn.can_recv(timeout=1) == False:
-		print('Something went wrong...')
-		exit(0)
 
 print(conn.recvline(keepends=False).decode('utf-8'))
 conn.close()
