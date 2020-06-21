4125754b2992cc2d0724fe31be519d81a98b6ba9
Willy Chiu <chiuwiy.cs08g@nctu.edu.tw>
2020-06-21 15:55:17 +0800

    Fix typo

--- asm/addsub1.asm
+++ asm/addsub1.asm
@@ -4,8 +4,9 @@
 ;     eax = eax - 0x38a4982
 ; ======
 ; ======
-; Enter your codes: (type 'done:' when done)mov eax, 0x11580a21
+; Enter your codes: (type 'done:' when done)
 
+mov eax, 0x11580a21
 add eax, 0x643ba8db
 sub eax, 0xc9aa6fd5
 done:
